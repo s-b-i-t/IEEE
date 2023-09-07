0bc3882a9fb5ce9aebf09982fc636a47504d1474
Shabit Bhatt <bhattshabit@gmail.com>
2023-09-07 10:45:04 -0400

    images updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import logo from './images/logo.jpg';
+import past1 from './images/past1.jpg';
+import past2 from './images/past2.jpg';
+import past3 from './images/past3.jpg';
+import upcoming1 from './images/upcoming1.jpg';
 
 const App = () => {
   const [showFirstText, setShowFirstText] = useState(false);
@@ -43,49 +47,39 @@ const App = () => {
       observer.disconnect();
     };
   }, [showUpcomingEvents, showPastEvents, showAboutUs]);
-
   
   const handleScroll = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
-    <div className="container">
-
-      {showFirstText && <h1 className="fade-in">IEEE PELS & PES </h1>}
-      
-      {showSecondText && <h2 className="fade-in">Welcome to the PELS & PES IEEE Chapter at the University of Connecticut</h2>}
-      
-      {showImage && <img src={logo} alt="Logo" className="fade-in" />}
-      
-
-      <div className="navbar">
+    <div className='container'>
+      {showFirstText && <h1 className='fade-in'>IEEE PELS & PES </h1>}
+      {showSecondText && <h2 className='fade-in'>Welcome to the PELS & PES IEEE Chapter at the University of Connecticut</h2>}
+      {showImage && <img src={logo} alt='Logo' className='fade-in' style={{width: '600px', height: 'auto'}} />}
+      <div className='navbar'>
         <button onClick={() => handleScroll(upcomingRef)}>Upcoming Events</button>
         <button onClick={() => handleScroll(pastRef)}>Past Events</button>
         <button onClick={() => handleScroll(aboutRef)}>About Us</button>
       </div>
-
       {showUpcomingEvents && (
-        <div id="upcoming-events" className="section" ref={upcomingRef}>
+        <div id='upcoming-events' className='section' ref={upcomingRef}>
           <h2>Upcoming Events</h2>
-          <p>Placeholder text for upcoming events.</p>
-          <img src={logo} alt="Logo"></img>
+          <img src={upcoming1} alt='Upcoming Event 1' style={{width: '600px', height: 'auto', marginBottom: '20px'}} />
         </div>
       )}
-
       {showPastEvents && (
-        <div id="past-events" className="section" ref={pastRef}>
+        <div id='past-events' className='section' ref={pastRef}>
           <h2>Past Events</h2>
-          <p>Placeholder text for past events.</p>
-          <img src={logo} alt="Logo"></img>
+          <img src={past1} alt='Past Event 1' style={{width: '600px', height: 'auto', marginBottom: '20px'}} />
+          <img src={past2} alt='Past Event 2' style={{width: '600px', height: 'auto', marginBottom: '20px'}} />
+          <img src={past3} alt='Past Event 3' style={{width: '600px', height: 'auto', marginBottom: '20px'}} />
         </div>
       )}
-
       {showAboutUs && (
-        <div id="about-us" className="section" ref={aboutRef}>
+        <div id='about-us' className='section' ref={aboutRef}>
           <h2>About Us</h2>
-          <p>Placeholder text for about us.</p>
-          <img src={logo} alt="Logo"></img>
+          {/* Placeholder for future content */}
         </div>
       )}
     </div>
