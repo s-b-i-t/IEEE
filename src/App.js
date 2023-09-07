856e21b03c902985aed5d9832298912cdc16571f
Shabit Bhatt <bhattshabit@gmail.com>
2023-09-07 10:17:05 -0400

    images updated

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import logo from './images/logo.jpg';
 const App = () => {
   const [showFirstText, setShowFirstText] = useState(false);
   const [showSecondText, setShowSecondText] = useState(false);
-  const [showThirdText, setShowThirdText] = useState(false);
+  const [showImage, setshowImage] = useState(false);
+  const [showUpcomingEvents, setShowUpcomingEvents] = useState(false);
+  const [showPastEvents, setShowPastEvents] = useState(false);
+  const [showAboutUs, setShowAboutUs] = useState(false);
+
   const upcomingRef = useRef(null);
   const pastRef = useRef(null);
   const aboutRef = useRef(null);
 
   useEffect(() => {
-    setTimeout(() => setShowFirstText(true), 1000);
-    setTimeout(() => setShowSecondText(true), 2000);
-    setTimeout(() => setShowThirdText(true), 3000);
+    setTimeout(() => setShowFirstText(true), 500);
+    setTimeout(() => setShowSecondText(true), 1000);
+    setTimeout(() => setshowImage(true), 1500);
+    setTimeout(() => setShowUpcomingEvents(true), 2500);
+    setTimeout(() => setShowPastEvents(true), 3500);
+    setTimeout(() => setShowAboutUs(true), 4500);
 
     const observer = new IntersectionObserver(
       (entries) => {
@@ -28,48 +35,59 @@ const App = () => {
       }
     );
 
-    observer.observe(upcomingRef.current);
-    observer.observe(pastRef.current);
-    observer.observe(aboutRef.current);
+    if (upcomingRef.current) observer.observe(upcomingRef.current);
+    if (pastRef.current) observer.observe(pastRef.current);
+    if (aboutRef.current) observer.observe(aboutRef.current);
 
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [showUpcomingEvents, showPastEvents, showAboutUs]);
 
+  
   const handleScroll = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div className="container">
-      {showFirstText && <h1 className="fade-in">PELS & PES IEEE Chapter</h1>}
-      {showSecondText && <h2 className="fade-in">Welcome to the PELS & PES IEEE Chapter at the University of Connecticut</h2>}
-      {showThirdText && <img src={logo} alt="Logo" className="fade-in" />}
+
+      {showFirstText && <h1 className="fade-in">IEEE PELS & PES </h1>}
       
+      {showSecondText && <h2 className="fade-in">Welcome to the PELS & PES IEEE Chapter at the University of Connecticut</h2>}
+      
+      {showImage && <img src={logo} alt="Logo" className="fade-in" />}
+      
+
       <div className="navbar">
         <button onClick={() => handleScroll(upcomingRef)}>Upcoming Events</button>
         <button onClick={() => handleScroll(pastRef)}>Past Events</button>
         <button onClick={() => handleScroll(aboutRef)}>About Us</button>
       </div>
 
-      <div id="upcoming-events" className="section" ref={upcomingRef}>
-        <h2>Upcoming Events</h2>
-        <p>Placeholder text for upcoming events.</p>
-        <img src={logo} alt="Logo"></img>
-      </div>
+      {showUpcomingEvents && (
+        <div id="upcoming-events" className="section" ref={upcomingRef}>
+          <h2>Upcoming Events</h2>
+          <p>Placeholder text for upcoming events.</p>
+          <img src={logo} alt="Logo"></img>
+        </div>
+      )}
 
-      <div id="past-events" className="section" ref={pastRef}>
-        <h2>Past Events</h2>
-        <p>Placeholder text for past events.</p>
-        <img src={logo} alt="Logo"></img>
-      </div>
+      {showPastEvents && (
+        <div id="past-events" className="section" ref={pastRef}>
+          <h2>Past Events</h2>
+          <p>Placeholder text for past events.</p>
+          <img src={logo} alt="Logo"></img>
+        </div>
+      )}
 
-      <div id="about-us" className="section" ref={aboutRef}>
-        <h2>About Us</h2>
-        <p>Placeholder text for about us.</p>
-        <img src={logo} alt="Logo"></img>
-      </div>
+      {showAboutUs && (
+        <div id="about-us" className="section" ref={aboutRef}>
+          <h2>About Us</h2>
+          <p>Placeholder text for about us.</p>
+          <img src={logo} alt="Logo"></img>
+        </div>
+      )}
     </div>
   );
 };
